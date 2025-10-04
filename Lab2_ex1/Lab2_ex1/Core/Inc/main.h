9d12beb0e49b292e7b47315aceb4e4c54ea37bdb
Dahlia1337 <vinhluongngo@gmail.com>
2025-10-04 16:39:20 +0700

    add Ex1

---
+++ Lab2_ex1/Lab2_ex1/Core/Inc/main.h
@@ -0,0 +1,40 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f1xx_hal.h"
+
+#define Led_red_Pin GPIO_PIN_5
+#define Led_red_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_6
+#define EN0_GPIO_Port GPIOA
+#define EN1_Pin GPIO_PIN_7
+#define EN1_GPIO_Port GPIOA
+#define Seg1_Pin GPIO_PIN_0
+#define Seg1_GPIO_Port GPIOB
+#define Seg2_Pin GPIO_PIN_1
+#define Seg2_GPIO_Port GPIOB
+#define Seg3_Pin GPIO_PIN_2
+#define Seg3_GPIO_Port GPIOB
+#define Seg4_Pin GPIO_PIN_3
+#define Seg4_GPIO_Port GPIOB
+#define Seg5_Pin GPIO_PIN_4
+#define Seg5_GPIO_Port GPIOB
+#define Seg6_Pin GPIO_PIN_5
+#define Seg6_GPIO_Port GPIOB
+#define Seg7_Pin GPIO_PIN_6
+#define Seg7_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
